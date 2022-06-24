339a20a933607f7f59926814a8de6205110a488f
KlaudiaBodyk <klaudia.bodyk@icloud.com>
2022-06-24 19:12:34 +0200

    Add second struct

--- main.cpp
+++ main.cpp
@@ -5,3 +5,18 @@ struct  BMPFileHeader{
     uint16_t reserved2{0};
        uint32_t offset_data{0};
 };
+
+struct  BMPInfoHeader{
+    uint32_t size{ 0 };
+    int32_t width{ 0 };
+    int32_t height{ 0 };
+    uint16_t planes{ 1 };
+    uint16_t bit_count{ 0 };
+    uint32_t compression{ 0 };
+    uint32_t size_image{ 0 };
+    int32_t x_pixels_per_meter{ 0 };
+    int32_t y_pixels_per_meter{ 0 };
+    uint32_t colors_used{ 0 };
+    uint32_t colors_important{ 0 };
+};
+
